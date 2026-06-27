23328ce39f40e1208dd250bd5b827d953f34c0f6
badjoke-lab <badjoke.lab@gmail.com>
2026-06-27 11:20:14 +0900

    Add evidence source groups A

---
+++ config/evidence-source-groups-a.mjs
@@ -0,0 +1,10 @@
+export const evidenceSourceGroupsA = [
+  ['sog_src_ust_luna_classic_recovery_arxiv', ['sog_src_ust_chain_halt_research_event'], 'https://arxiv.org/abs/2207.01700'],
+  ['sog_src_ust_failure_research_arxiv', ['sog_src_ust_lfg_research_event'], 'https://arxiv.org/abs/2207.13914'],
+  ['sog_src_sdai_routes_batch_h', ['sog_src_susds_routes_batch_h'], 'https://developers.sky.money/quick-start/protocol-token-routes/'],
+  ['sog_src_usde_ethena_docs', ['sog_src_usde_ethena_lifecycle_event', 'sog_src_susde_overview_batch_h'], 'https://docs.ethena.fi/'],
+  ['sog_src_lusd_liquity_docs', ['sog_src_lusd_liquity_lifecycle_event'], 'https://docs.liquity.org/'],
+  ['sog_src_rai_faq_batch_b', ['sog_src_rai_settlement_batch_b'], 'https://docs.reflexer.finance/faq'],
+  ['sog_src_makerdao_docs_dai', ['sog_src_sky_dai_product_docs', 'sog_src_dai_mcd_2019_event'], 'https://docs.sky.money/'],
+  ['sog_src_susd_synthetix_docs', ['sog_src_susd_synthetix_lifecycle_event'], 'https://docs.synthetix.io/']
+];
